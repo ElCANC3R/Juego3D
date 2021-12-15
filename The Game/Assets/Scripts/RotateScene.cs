f77e6273f2d57078fe8225796f9808e6c8d71521
ElCANC3R <diejoruiz1993@gmail.com>
2021-12-14 18:46:59 -0600

    Bananas

--- The Game/Assets/Scripts/RotateScene.cs
+++ The Game/Assets/Scripts/RotateScene.cs
@@ -13,6 +13,6 @@ public class RotateScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0,2.5f,0));
+        transform.Rotate(new Vector3(0,0.1f,0));
     }
 }
